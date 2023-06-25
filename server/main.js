09a1b0e82c09ab568be09713940a3255642b9baf
bwsunkist <54732163+bwsunkist@users.noreply.github.com>
2023-06-25 18:13:26 +0900

    add auth_request sample

--- server/main.js
+++ server/main.js
@@ -8,6 +8,23 @@ var server = app.listen(3000, function(){
     console.log('express start. port:' + server.address().port);
 });
 
-app.get('/internal/home', function(req, res){
-  res.send('get /internal/home success!');
+app.post('/internal/home', function(req, res){
+  const authHeader = req.headers.authorization;
+
+  if (authHeader === "accessToken_home") {
+    res.send('get /internal/home success!');
+  } else {
+    res.status(401).send('Unauthorized /internal/home')
+  }
+});
+
+app.get('/internal/auth', function(req, res){
+  const authHeader = req.headers.authorization;
+
+  if (authHeader === "accessToken_auth") {
+    res.setHeader('Authorization', 'accessToken_home')
+    res.send('get /internal/auth success!');
+  } else {
+    res.status(401).send('Unauthorized /internal/auth')
+  }
 });
